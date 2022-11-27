10daf0d7878507f77ac8ede0db9b66cdff5d5fa4
Kestrel Kosdlic <70953258+KestrelYTReal@users.noreply.github.com>
2022-11-27 07:39:41 -0500

    Upload Escape From Hell Build 0.2.19174 for backup
    lol yeah almost lost all this work yesterday because pc had a stroke but we good now

---
+++ _CreateBuildDir.bat
@@ -0,0 +1,21 @@
+rmdir Build /s/q
+mkdir Build
+
+copy "Server.exe" "Build/Server.exe"
+xcopy "core" "Build/core/" /s/i/y
+xcopy "db" "Build/db/" /s/i/y
+xcopy "res" "Build/res/" /s/i/y
+xcopy "src" "Build/src/" /s/i/y
+xcopy "user" "Build/user/" /s/i/y
+
+
+rmdir "Build/user/cache/" /s/q
+rmdir "Build/user/events/" /s/q
+rmdir "Build/user/logs/" /s/q
+rmdir "Build/user/profiles/" /s/q
+rmdir "Build/user/mods" /s/q
+
+del "Build\user\configs\gameplay.json" /q
+del "Build\user\configs\server.json" /q
+
+pause
