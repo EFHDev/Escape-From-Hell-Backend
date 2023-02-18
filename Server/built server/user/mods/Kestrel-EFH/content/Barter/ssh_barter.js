dad77541535856bfd1396df79d9391a394acb551
Kestrel Kosdlic <70953258+EFHDev@users.noreply.github.com>
2023-02-18 02:54:42 -0500

    Uploaf better item making thingie - ssh_barter
    ye

--- Server/built server/user/mods/Kestrel-EFH/content/Barter/ssh_barter.js
+++ Server/built server/user/mods/Kestrel-EFH/content/Barter/ssh_barter.js
@@ -1,34 +1,35 @@
 "use strict";
+
+const db = DatabaseServer.tables;
+const localessource = db.locales.global;
+const item = db.templates.items
+const tradersource = db.traders;
 const ragfair = "ragfair"; //ragfair ID
 const lineman = "5a7c2eca46aef81a7ca2145d"; //trader id
-const db = DatabaseServer.tables;
-const item = db.templates.items
-const d_trader = db.traders;
-const d_locales = db.locales.global;
 
 class sshThing
 {
     static onLoadMod()
     {
         sshThing.CreateSSH();
-        sshThing.AddItemToTraders();
+        sshThing.Yeetbitchintotrader();
     }
     
     static CreateSSH()
     {
 		logger.logDebug("Creating SSH Barter Item") //For debugging purposes
-        let pk = JsonUtil.clone(item["59e3658a86f7741776641ac4"]); 
-        pk._id = "efh_ssh_barter"; //ID of the item stoopid
-        pk._props.Weight = 0.447; //Self explantory for the rest
-		pk._props.Width = 1;
-		pk._props.Height = 2;
-        pk._props.Prefab.path = "assets/content/items/friend-items/ssh/efh_ssh_barter.bundle";
-        item["efh_ssh_barter"] = pk;
+        let item = JsonUtil.clone(item["59e3658a86f7741776641ac4"]); 
+        item._id = "efh_ssh_barter"; //ID of the item stoopid
+        item._props.Weight = 0.447; //Self explantory for the rest
+		item._props.Width = 1;
+		item._props.Height = 2;
+        item._props.Prefab.path = "assets/content/items/friend-items/ssh/efh_ssh_barter.bundle";
+        item["efh_ssh_barter"] = item;
 
-        for (const localeID in d_locales)
+        for (const localeID in localessource)
         {
-            d_locales[localeID].templates["efh_ssh_barter"] = {
-                "Name": "<color=#FFFFFF>[EFH] Servph Corp PMC Group 1-1 Nikita Obliterators Helicopter Figureine</color>",
+            localessource[localeID].templates["efh_ssh_barter"] = {
+                "Name": "<color=#f51717>[EFH] Servph Corp PMC Group 1-1 Nikita Obliterators Helicopter Figureine</color>",
                 "ShortName": "Helicopter Figureine",
 				"Description": "The small figurine from Servph Corpp PMC Group is a meticulously crafted model depicting the fearsome Nikita Obliterators in action. The figurine features a highly detailed helicopter branded with the Servph Corpp Task Force 1-1 logo, soaring forward with intense speed and agility. A skilled sniper is shown aiming a powerful rifle out of the helicopter, ready to take down any target with ruthless precision. This figurine is a perfect representation of the highly trained, specialized mercenaries of the Servph Corpp PMC Group, who are renowned for their strategic prowess and combat skills. It is a must-have for military enthusiasts and collectors who appreciate the skill and bravery of these elite soldiers."
             };
@@ -42,7 +43,7 @@ class sshThing
         });
     }
 
-    static AddItemToTraders() //Add to flea and other traders too ig.
+    static Yeetbitchintotrader() //Add to flea and other traders too ig.
     {
         const assort_items = {
             "_id": "efh_ssh_barter",
@@ -62,9 +63,9 @@ class sshThing
 				"_tpl": "5449016a4bdc2d6f028b456f"
 			}]
         ];
-        d_trader[ragfair].assort.items.push(assort_items);
-        d_trader[ragfair].assort.barter_scheme["efh_ssh_barter"] = barter_scheme
-        d_trader[ragfair].assort.loyal_level_items["efh_ssh_barter"] = 1;
+        tradersource[ragfair].assort.items.push(assort_items);
+        tradersource[ragfair].assort.barter_scheme["efh_ssh_barter"] = barter_scheme
+        tradersource[ragfair].assort.loyal_level_items["efh_ssh_barter"] = 1;
     }
 }
 
