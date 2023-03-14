3cda142098b498d3c5014f507e40aa1453d6002d
Kestrel Kosdlic <70953258+EFHDev@users.noreply.github.com>
2023-03-14 01:11:01 -0400

    Revert "Update initializer.js"
    This reverts commit 560fbc0d7ecc801f074689f03daa0245cd11d9d4.

--- Server/core/initializer.js
+++ Server/core/initializer.js
@@ -8,7 +8,7 @@ class Initializer {
   constructor() {
 <<<<<<< HEAD
     if (config.AutoUpdateEFH === true) {
-      console.log(chalk.red.bold(" [EFH] Auto-Update will clear any changes you make to your servers Database (I.E changing boss spawns or changing max ammo stacks, that isnt done through a mod, Please, if you dont want this, turn this off. This does not effect profiles!"))
+      console.log(chalk.red.bold(" [EFH] Auto-Update will clear any changes you make to your servers Database (I.E changing boss spawns or changing max ammo stacks, that isnt done through a mod, Please, if you dont want this, turn this off."))
       try {
         downloadReleaseTag('EFHDev', 'Escape-From-Hell-PoC', 'Server', 'Server');
       }
